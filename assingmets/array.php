8dfedb61607a2817d0fad1a4061034376f67dc35
Mwas <109804319+blackeyedpuppy@users.noreply.github.com>
2022-08-22 12:36:34 +0300

    Edited array.php

--- assingmets/array.php
+++ assingmets/array.php
@@ -1,22 +1,37 @@
 <?php
 //1 dimensional array
-//$data = array();
-$data[] = "Mwathani";
-$data[] = 15;
-echo " $data[1] <br>";
-echo count($data)
-
+$favcolor = array("red","black","purple", "blue");
+echo "My fav color is ".$favcolor[1]."\n";
+echo "and I have ".count($favcolor)." other colors in my list.\n";
+$favcolor[2] = "White";
+echo "My second favorite color must be snow ".$favcolor[2]."\n";
+echo "<br";
 //2 dimensional array
+$gang = array(
+    array("Max",19,2003),
+    array("Jerry",18,2004),
+    array("Brent",27,1995),
+    array("Niko",23,1999)
+);
 
-
-
+echo "The youngest person is ".$gang[1][0]."and his age is".$gang[1][1]."\n";
 //sort
-
-
+echo"<br>";
+sort($favcolor);
+for ($i=0; $i <count($favcolor) ; $i++) { 
+    echo "$favcolor[$i]\n";
+    echo "<br>";
+}
 
 
 //rsort
-
+echo "<br>";
+$year = array(2001,2020,2018,2014);
+rsort($year);
+for ($z=0; $z < count($year) ; $z++) { 
+    echo "$year[$z]\n";
+    echo "<br>";
+};
 
 
 
